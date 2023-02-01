1872e49017c07155d2db80da1a24724cadea5144
Aly Mohamed <aly.mohamed@ucalgary.ca>
2023-01-31 23:27:22 -0700

    added the trip
    Trip is complete but not tested

--- Assignment1/edu/ucalgary/oop/Itinerary.java
+++ Assignment1/edu/ucalgary/oop/Itinerary.java
@@ -11,10 +11,14 @@ public class Itinerary {
     // Returns a string in the format of:
     // value (key)
     public static String fmtString(String key, String value) {
+        // needs to take the key and value, where the value is the city, and the key is that it is a city, for example:
+        // Paris (City) or Calgary (City) or Canada (Country) value (key)
+        return String.format(value, key)
     }
 
     // Constructor
     public Itinerary(String[][] myTrips) {
+
     }
 
     // Getter
@@ -22,6 +26,7 @@ public class Itinerary {
     }
 
     public String formatByArrival() {
+        
     }
 
     // The first array holds years (2021-2023).
--- Assignment1/edu/ucalgary/oop/Trip.java
+++ Assignment1/edu/ucalgary/oop/Trip.java
@@ -14,6 +14,9 @@ public class Trip {
     // Returns a string in the format of:
     // value (key)
     public static String fmtString(String key, String value) {
+        // needs to take the key and value, where the value is the city, and the key is that it is a city, for example:
+        // Paris (City) or Calgary (City) or Canada (Country) value (key)
+        return String.format(value, key)
     }
 
     // Constructor
@@ -22,48 +25,61 @@ public class Trip {
 
     // Given a date string, return just the year
     public static int getYear(String date) {
+        int year = date.substring(0, 4);
+        return year;
     }
 
     // Given a date string, return just the month
     // Since it is an int, a date like "2022-01-12" returns 1
     public static int getMonth(String date) {
+        int month = date.substring(5, 7);
+        return month;
     }
 
     // Return a formatted string of key/value pairs, with commas
     // between each. See the output for an example.
     public String formatTrip() {
+        return String.format("%s (Arrival), %s(Departure), %s(City), %s(Country)", this.arrival, this.departure, this.city, this.country);
     }
 
     // Getter
     public String getArrival() {
+        return this.arrival;
     }
 
     // Getter
     public String getDeparture() {
+        return this.departure;
     }
 
     // Getter
     public String getCity() {
+        return this.city;
     }
 
     // Getter
     public String getCountry() {
+        return this.country;
     }
 
     // Setter
     public void setArrival(String date) {
+        this.arrival =  date;
     }
 
     // Setter
     public void setDeparture(String date) {
+        this.setDeparture = date;
     }
 
     // Setter 
     public void setCity(String city) {
+        this.city = city;
     }
 
     // Setter 
     public void setCountry(String country) {
+        this.country = country;
     }
 
 }
